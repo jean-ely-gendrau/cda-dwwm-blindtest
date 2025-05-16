0ef90d89d5fbe9ffaa08def692bf633d6d17579a
Ilian Metina <ilian.metina83@gmail.com>
2025-05-16 15:33:19 +0200

    Ajout fichier page.tsx avec fonction useEffect

--- app-nextjs/src/app/page.tsx
+++ app-nextjs/src/app/page.tsx
@@ -1,13 +1,40 @@
+'use client'
 import Hero from "@/component/hero";
+import React, {useEffect, useState} from 'react';
+import { PlayerInterfaceDTO } from "@/interfaces/ProcessPlayer";
+
+/* Les import en dessous sont pour voir le rendu, à supprimer */
+import Footer from "@/component/layout/Footer";
+import NavBar from "@/component/layout/Header";
+import PlayerDTO from "@/dto/PlayerDTO";
+
 
 export default function Home(){
+
+  const [player, setPlayer] = useState<PlayerDTO>();
+
+  useEffect(() => {
+
+  
+  
+        const response = await fetch("http://localhost:3000/api/v1/game/history");
+        setPlayer(await response.json());
+        console.log(player);
+        console.log(response);
+      
+      
+    
+    
+  }, []);
 
   return(
 
  
     <main>
-
+      <p>salut {player?.username}</p>
+      
       <Hero />
+
       
     </main>
   )
