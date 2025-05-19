b400dcaaa27c0e5f47f90538446b63eb6a541811
Ilian Metina <ilian.metina83@gmail.com>
2025-05-19 15:35:50 +0200

    Ajout header/footer

---
+++ app-nextjs/src/component/layout/Footer.tsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+import React from 'react';
+
+export default function Footer(){
+
+    return(
+
+        <footer className="footer">
+            <p className="copyrights"> © 2025 BlindTest - Tous droits réservés</p>
+        </footer>
+    )
+}
+
+/* A importer dans le fichier correspondant */
